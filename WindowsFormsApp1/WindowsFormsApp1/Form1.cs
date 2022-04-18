83c194bb6739bdad2d184c6b5a5e2cfff6f1ca99
kirankommi <kirankumar.kommi@gmail.com>
2022-04-18 17:53:03 +0530

    windowsapp

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            txtOutput.Text = Math(txtInput.Text, txtInput2.Text, "addition");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            txtOutput.Text = Math(txtInput.Text, txtInput2.Text, "subtraction");
+        }
+
+        string Math(string str, string str2,string mathtype)
+        {
+            int input = int.Parse(str);
+            int input2 = int.Parse(str2);
+            int output = 0;
+            if(mathtype == "addition")
+            {
+                output = input + input2;
+            }
+            else if(mathtype == "subtraction")
+            {
+                output = input - input2;
+            }
+
+            return output.ToString();
+        }
+    }
+}
